1a6668e8e5d703ab1b96001fd0e07e5f1ce20682
vbmartinez4 <vbmartinez15@gmail.com>
2022-07-15 19:39:39 -0400

    Data Analysis on Queries.

--- schema.sql
+++ schema.sql
@@ -43,7 +43,7 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_emp (
