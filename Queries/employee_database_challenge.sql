37de5c8825069c27b89e739288b47133b52fc093
Vannesa Martinez <vbmartinez15@gmail.com>
2022-07-16 13:33:06 -0400

    Updated SQL Code

--- Queries/employee_database_challenge.sql
+++ Queries/employee_database_challenge.sql
@@ -47,3 +47,47 @@ ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01')	
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no;
+
+-- Create new table for count of mentor employees by title
+SELECT COUNT (me.emp_no), me.title
+INTO mentorship_titles
+FROM mentorship_eligibilty as me
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT COUNT(emp_no) FROM unique_titles
+SELECT COUNT(emp_no) FROM employees
+SELECT COUNT(emp_no) FROM mentorship_eligibilty
+
+-- Create new table for count of retiree genders
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+e.gender,
+tl.title,
+tl.from_date,
+tl.to_date
+INTO retirement_genders
+FROM employees as e
+INNER JOIN titles as tl
+ON (e.emp_no = tl.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) rg.emp_no,
+rg.first_name,
+rg.last_name,
+rg.gender,
+rg.title
+INTO gender_titles
+FROM retirement_genders as rg
+WHERE rg.to_date = ('9999-01-01')
+ORDER BY rg.emp_no ASC, rg.to_date DESC;
+
+-- Create new table for count of retiring employees by gender
+SELECT COUNT (gt.emp_no), gt.gender
+--INTO retiring_titles
+FROM gender_titles as gt
+GROUP BY gender
+ORDER BY COUNT(gender)DESC;
