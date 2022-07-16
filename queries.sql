50aa58aa2f090c100785e71619661375c6701cd1
vbmartinez4 <vbmartinez15@gmail.com>
2022-07-15 23:07:17 -0400

    Creating Tailored Lists.

--- queries.sql
+++ queries.sql
@@ -95,3 +95,85 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, 
+	first_name, 
+last_name
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE emp_info CASCADE
+
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+	
+-- List of managers per department
+SELECT dm.dept_no,
+		d.dept_name,
+		dm.emp_no,
+		ce.last_name,
+		ce.first_name,
+		dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		on (dm.emp_no = ce.emp_no);
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+--INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON(de.dept_no = d.dept_no);
+
+-- Create new table for sales team
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+d.dept_name
+INTO sales_team_info
+FROM retirement_info as ri
+INNER JOIN  dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+-- Create new table for sales and development
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+d.dept_name
+INTO sales_develop_team_info
+FROM retirement_info as ri
+INNER JOIN  dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN('Sales','Development');
